2f12113402e2fc2c6b3f563a68e89c4bb9f4ed79
skeli74 <peev@gbg.bg>
2022-09-25 11:22:30 +0200

    add solution

---
+++ data_types_and_variables/data_types_and_variables_lab_exercise/party_profit.py
@@ -0,0 +1,20 @@
+group_size = int(input())
+days_of_the_adventure = int(input())
+coins = 0
+companions = 0
+companions += group_size
+for days in range(1, days_of_the_adventure + 1):
+    coins += 50
+    coins -= companions * 2
+    if days % 10 == 0:
+        companions -= 2
+    if days % 15 == 0:
+        companions += 5
+    if days % 3 == 0:
+        coins -= companions * 3
+    if days % 5 == 0:
+        coins += 20 * companions
+        if days % 3 == 0:
+            coins -= companions * 2
+result = int(coins / companions)
+print(f"{companions} companions received {result} coins each.")
